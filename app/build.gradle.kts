b5302598bdc33218463490c92c663e510c7475f9
JoseAntonioGarciaRdz <garciajoa@globalhitss.com>
2026-08-17 10:35:05 -0600

    [Update] Lib Paging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation(libs.androidx.paging.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
